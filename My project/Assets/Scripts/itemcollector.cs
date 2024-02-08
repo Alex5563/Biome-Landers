6b55fbc5a6f09c47408eec05589167e35e9afad5
Alex5563 <122497151+Alex5563@users.noreply.github.com>
2024-02-07 19:15:06 -0500

    added coins to jungle

--- My project/Assets/Scripts/itemcollector.cs
+++ My project/Assets/Scripts/itemcollector.cs
@@ -22,7 +22,7 @@ public class itemcollector : MonoBehaviour
 	  }
 	  if (coins == 0)
 	  {
-		coinsText.text = "Go to the Middle for the Next World";
+		coinsText.text = "Go to the Middle for the Next Biome";
 		teleporter.SetActive(true);
 	  }
   }
