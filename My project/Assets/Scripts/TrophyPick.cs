bb61d888779254baffd0c03e6613fcf2d7d06135
soipsuo <an.hamdan2009@gmail.com>
2024-02-14 21:43:09 -0500

    Trophy Room

---
+++ My project/Assets/Scripts/TrophyPick.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CupScipt : MonoBehaviour
+{
+
+    public static int secretcoins;
+    public GameObject bronzetrophy;
+    public GameObject silvertrophy;
+    public GameObject goldtrophy;
+
+    [SerializeField] AudioSource coinSound;
+
+    void Start()
+    {
+        if (secretcoins == 4) {
+            goldtrophy.SetActive(true);
+        }
+        else if (secretcoins == 2 || secretcoins == 3) {
+            silvertrophy.SetActive(true);
+        }
+        else {
+            bronzetrophy.SetActive(true);
+        }
+
+    }
+      private void OnTriggerEnter(Collider other)
+  {
+	  if (other.gameObject.CompareTag("Trophy"))
+	  {
+		  Destroy(other.gameObject);
+		  coinSound.Play();
+	  }
+  }
+}
--- My project/Assets/Scripts/itemcollector.cs
+++ My project/Assets/Scripts/itemcollector.cs
@@ -7,18 +7,22 @@ using TMPro;
 
 public class itemcollector : MonoBehaviour
 {
-	  int coins = 1;
+	  public static int coins;
 
 	  [SerializeField] TextMeshProUGUI coinsText;
 	  [SerializeField] AudioSource coinSound;
+
+	  void Start(){
+		coinsText.text = "Secret Coins: " + coins + "/4";
+ 	 }
 
   private void OnTriggerEnter(Collider other)
   {
 	  if (other.gameObject.CompareTag("Coin"))
 	  {
 		  Destroy(other.gameObject);
-		  coins--;
-		  coinsText.text = "Secret Coin: " + coins;
+		  coins += 1;
+		  coinsText.text = "Secret Coins: " + coins + "/4";
 		  coinSound.Play();
 	  }
   }
